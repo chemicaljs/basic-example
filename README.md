f36917bf17071c39a9115c4de357eee570472422
Nebelung <nebelung@mailfence.com>
2024-08-31 06:25:32 +0000

    Upload example

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
-# basic-example
+# Basic Example
 A simple example using Chemical.
+
+## Setup
+First, clone the GitHub repo.
+
+```bash
+git clone https://github.com/chemicaljs/basic-example.git
+```
+
+Next, run `npm install` to install dependencies.
+
+Finally run `npm start` to start the app on `localhost:3000`.
+
+## License
+Basic Example uses the AGPL 3.0 license.
---
+++ index.js
@@ -0,0 +1,21 @@
+import { ChemicalServer } from "chemicaljs";
+import express from "express";
+
+const [app, listen] = new ChemicalServer();
+const port = process.env.PORT || 3000;
+
+app.use(express.static("public", {
+    index: "index.html",
+    extensions: ["html"]
+}));
+
+app.serveChemical();
+
+app.use((req, res) => {
+    res.status(404);
+    res.send("404 Error");
+});
+
+listen(port, () => {
+    console.log(`Basic example listening on port ${port}`);
+});
